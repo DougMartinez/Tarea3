2ace93e6f176c271bfb8c86e8e6dd3d6bca13c7a
Douglas <Douglas@Douglas>
2017-08-07 18:21:00 -0600

    Parte de Palíndromos

--- src/tarea_3/Tarea_3.java
+++ src/tarea_3/Tarea_3.java
@@ -10,77 +10,119 @@ import java.util.Scanner;
 public class Tarea_3 {
 
     static Scanner scan = new Scanner(System.in);
-    static String[] usuarios = {"Vacío 1","Vacío 2","Vacío 3","Vacío 4","Vacío 5"};
+    static String[] usuarios = {"Vacío","Vacío","Vacío","Vacío","Vacío"};
+    static int opu = 0;
+    static int op = 0;
+    static int aux = 0;
+    static int nel = 0;
     
     public static void main(String[] args) {
-        int op;
         System.out.println("Tarea 3\n201603168");
         do {
-            System.out.println("\nElije una opción:");
-            System.out.println("1. Usuarios \n2. Palabras Palindromas\n3. Salir");
-            op = scan.nextInt();
-            System.out.println();
-            switch (op){
-                case 1:
-                    usuarios();
+            if (nel == 1){
+                break;
+            } else {    
+                System.out.println("\nElije una opción:");
+                System.out.println("1. Usuarios \n2. Palabras Palindromas\n3. Salir");
+                op = scan.nextInt();
+                if (op == 3){
+                    System.out.println("Adios y gracias");
                     break;
-                case 2:
-                    palabras();
-                    break;
-                case 3:
-                    System.out.println("Adiós y gracias");
-                    break;
-                default:
-                    System.out.println("Opción incorrecta, prueba otra vez.\n");
-            }
-        } while (op != 3);
-    }
-        
-    private static void usuarios(){
-        int opu;
-        String name;
-        System.out.println("Menú de Usuarios");
-        do {    
-            System.out.println("\nElije una opción:");
-            System.out.println("1. Ingresar Usuarios \n2. Mostrar todos los usuarios\n3. Mostrar usuario personalizado\n4. Menú Principal\n5. Salir");
-            opu = scan.nextInt();
-            System.out.println();
-                switch (opu){
+                }
+                System.out.println();
+                switch (op){
                     case 1:
-                        System.out.println("Ingrese los 5 usuarios");
-                        for(int x=0; x<=4; x++){
-                            usuarios[x] = scan.nextLine();
-                        };
+                        usuarios();
                         break;
                     case 2:
-                        System.out.println("Mostrar todos los usuarios");
-                        for(int y=0; y<=4; y++){
-                            System.out.println((y + 1) + ". " + usuarios[y] );
-                        };
-                        break;
-                    case 3:
-                        System.out.println("Mostar Usuario Personalizado\n Ingresar Usuario");
-                        name = scan.nextLine();
-                        for(int z=0; z<=4; z++){
-                            if(usuarios[z].equalsIgnoreCase(name)){
-                                System.out.println("Usuario\n" + usuarios[z]);
-                            } else {
-                                System.out.println("ERROR, No existe ninguna coincidencia");
-                            };
-                        };
-                        break;
-                    case 4:
-                        break;
-                    case 5:
-                        System.out.println("Adiós y gracias");
+                        palabras();
                         break;
                     default:
                         System.out.println("Opción incorrecta, prueba otra vez.\n");
                 }
-        } while (opu != 4 || opu == 5);
+            }
+            break;
+        } while ( op != 3);
+    }
+        
+    private static void usuarios(){
+        String name;
+        System.out.println("Menú de Usuarios");
+        do {    
+            System.out.println("\nElije una opción:");
+            System.out.println("1. Ingresar Usuarios \n2. Mostrar todos los usuarios\n3. Mostrar usuario personalizado\n4. Regresar a Menú Principal\n5. Salir");
+            opu = scan.nextInt();
+            System.out.println();
+                if (opu == 5){
+                    nel = 1;
+                    break;
+                } else {
+                    switch (opu){
+                        case 1:
+                            System.out.println("Ingrese los 5 usuarios");
+                            scan.nextLine();
+                            for(int x=0; x<=4; x++){
+                                usuarios[x] = scan.nextLine();
+                            };
+                            break;
+                        case 2:
+                            System.out.println("Mostrar todos los usuarios");
+                            for(int y=0; y<=4; y++){
+                                System.out.println((y + 1) + ". " + usuarios[y] );
+                            };
+                            break;
+                        case 3:
+                            System.out.println("Mostar Usuario Personalizado\nIngresar Usuario");
+                            name = scan.next();
+                            System.out.println();
+                            for(int z=0; z<=4; z++){
+                                if(usuarios[z].equalsIgnoreCase(name)){
+                                    aux = aux + 1;
+                                }
+                            }
+                            if( aux > 0){
+                                System.out.println("Usuario encontrado:");
+                                System.out.println(name);
+                            } else {
+                                System.out.println("ERROR, No existe ninguna coincidencia");
+                            };
+                            break;
+                        case 4:
+                            System.out.println("Bandera");
+                            break;
+                        case 5:
+                            nel = 1;
+                            break;
+                        default:
+                            System.out.println("Opción incorrecta, prueba otra vez.\n");
+                    }
+                    break;
+                }
+        } while (opu != 5);
     }
     
     private static void palabras(){
-        
+        String pal;
+        int inc = 0;
+        boolean bError = false;
+        System.out.println("Palabras Palindromas\nIngresar Palabra");
+        scan.nextLine();
+        pal = scan.nextLine();
+        if (palindromo(pal))
+            System.out.println(pal + " es palindromo");
+        else
+            System.out.println(pal + " no es palindromo");
+    }
+    
+    static boolean palindromo(String palabra){
+        String modificado = palabra.replaceAll(" ","").toLowerCase();
+        int i=0;
+        while (i < modificado.length() / 2)
+        {
+            if (modificado.charAt(i) != modificado.charAt(modificado.length() -1 -i))
+                return false;
+            i++;
+        }
+        return true;
     }
 }
